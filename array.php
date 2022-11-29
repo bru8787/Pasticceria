7076d0afd00d97a3257237b034e78f37eaa44889
bru8787 <bruno.ndini@gmail.com>
2022-11-29 19:17:22 +0100

    il mio primo invio

---
+++ array.php
@@ -0,0 +1,161 @@
+<?php
+    $Dolci = [
+        [
+            "DolceName" => "BABBA'",
+            "DolcePic" => 'baba.png',
+            "DolcePrezzo" => '3.5€',
+            "DolceTesto" => 'Partiamo dal suono della parola, così importante per i napoletani ai quali interessa sempre anche la musicalità di una frase e di un discorso, meglio se accompagnata da gesti didascalici con le mani e ammiccamenti con gli occhi. Già, una comunicazione a tutto campo con la propria fisicità, tipica delle grandi città di un tempo visitate da genti straniere con le quali commerciare e fare affari, esigenze inutili se invece si era costretti a vivere nei campi o con le pecore o in piccoli borghi e cittadine fuori dalle rotte. Babà ha dunque nel suo suono, la seconda bi appena appena un po’ raddoppiata ma senza calcare sicché mai diventa babbà ma non è neanche babà alla francese, uno dei segreti capaci di annunciare il suo successo perchè gioca sulla piacevolezza dell’udito e sul successo della memoria: come dimenticare mai qualcosa, un dolce, con questo nome? Le prime due lettere dell’alfabeto, talmente facili da pronunciare che per i greci erano sinonimo dei popoli privi di una lingua costruita, bar bar, barbari appunto.',
+            "DolceQty" => '30',
+        ],
+
+        [
+            "DolceName" => "Ciambella",
+            "DolcePic" => 'ciambella.jpeg',
+            "DolcePrezzo" => '4€',
+            "DolceTesto" => 'La ciambella è definita come un dolce di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "DolceQty" => '40',
+        ],
+        [
+            "DolceName" => "Ciambella",
+            "DolcePic" => 'ciambella.jpeg',
+            "DolcePrezzo" => '4€',
+            "DolceTesto" => 'La ciambella è definita come un dolce di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "DolceQty" => '40',
+        ],
+        [
+            "DolceName" => "Ciambella",
+            "DolcePic" => 'ciambella.jpeg',
+            "DolcePrezzo" => '4€',
+            "DolceTesto" => 'La ciambella è definita come un dolce di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "DolceQty" => '40',
+        ],
+        [
+            "DolceName" => "Ciambella",
+            "DolcePic" => 'ciambella.jpeg',
+            "DolcePrezzo" => '4€',
+            "DolceTesto" => 'La ciambella è definita come un dolce di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "DolceQty" => '40',
+        ],
+        [
+            "DolceName" => "Ciambella",
+            "DolcePic" => 'ciambella.jpeg',
+            "DolcePrezzo" => '4€',
+            "DolceTesto" => 'La ciambella è definita come un dolce di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "DolceQty" => '40',
+        ],
+        [
+            "DolceName" => "Ciambella",
+            "DolcePic" => 'ciambella.jpeg',
+            "DolcePrezzo" => '4€',
+            "DolceTesto" => 'La ciambella è definita come un dolce di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "DolceQty" => '40',
+        ],
+        [
+            "DolceName" => "Ciambella",
+            "DolcePic" => 'ciambella.jpeg',
+            "DolcePrezzo" => '4€',
+            "DolceTesto" => 'La ciambella è definita come un dolce di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "DolceQty" => '40',
+        ],
+
+
+        [
+            "DolceName" => "Cannolo siciliano",
+            "DolcePic" => 'cannollo.png',
+            "DolcePrezzo" => '5€',
+            "DolceTesto" => 'La storia del cannolo siciliano è molto antica e, come spesso accade per storie o leggende tramandate oralmente, non si è certi della sua effettiva origine.Per molti la nascita di questo delizioso dolce tipico della tradizione siciliana ha origini antiche, che risalgono addirittura all’epoca romana, attorno al 70 a.C. Molti ritengono che l’antica ricetta romana venne rivisitata dai saraceni che vissero in Sicilia, rendendo il tutto più dolce e sfizioso grazie alle importazioni di alcuni prodotti della loro cucina, come la cannella, i pistacchi ed il miele. Altri ancora sostengono che sì, l’origine sia araba, ma che la ricetta venne rivisitata e riproposta dalle suore di un convento per celebrare il carnevale, divenendo un dolce tipico della festa. Oggi il cannolo siciliano si può trovare tutti giorni e in ogni pasticceria della Sicilia.Una cosa di cui si ha certezza, rispetto alle origini di questo dolce, è l’etimologia: il nome proviene dalle canne di fiume in cui anticamente veniva avvolto l’impasto prima della cottura, da qui il nome cannolo.',
+            "DolceQty" => '50',
+        ],
+        [
+            "DolceName" => "Cannolo siciliano",
+            "DolcePic" => 'cannollo.png',
+            "DolcePrezzo" => '5€',
+            "DolceTesto" => 'La storia del cannolo siciliano è molto antica e, come spesso accade per storie o leggende tramandate oralmente, non si è certi della sua effettiva origine.Per molti la nascita di questo delizioso dolce tipico della tradizione siciliana ha origini antiche, che risalgono addirittura all’epoca romana, attorno al 70 a.C. Molti ritengono che l’antica ricetta romana venne rivisitata dai saraceni che vissero in Sicilia, rendendo il tutto più dolce e sfizioso grazie alle importazioni di alcuni prodotti della loro cucina, come la cannella, i pistacchi ed il miele. Altri ancora sostengono che sì, l’origine sia araba, ma che la ricetta venne rivisitata e riproposta dalle suore di un convento per celebrare il carnevale, divenendo un dolce tipico della festa. Oggi il cannolo siciliano si può trovare tutti giorni e in ogni pasticceria della Sicilia.Una cosa di cui si ha certezza, rispetto alle origini di questo dolce, è l’etimologia: il nome proviene dalle canne di fiume in cui anticamente veniva avvolto l’impasto prima della cottura, da qui il nome cannolo.',
+            "DolceQty" => '50',
+        ],
+        [
+            "DolceName" => "Cannolo siciliano",
+            "DolcePic" => 'cannollo.png',
+            "DolcePrezzo" => '5€',
+            "DolceTesto" => 'La storia del cannolo siciliano è molto antica e, come spesso accade per storie o leggende tramandate oralmente, non si è certi della sua effettiva origine.Per molti la nascita di questo delizioso dolce tipico della tradizione siciliana ha origini antiche, che risalgono addirittura all’epoca romana, attorno al 70 a.C. Molti ritengono che l’antica ricetta romana venne rivisitata dai saraceni che vissero in Sicilia, rendendo il tutto più dolce e sfizioso grazie alle importazioni di alcuni prodotti della loro cucina, come la cannella, i pistacchi ed il miele. Altri ancora sostengono che sì, l’origine sia araba, ma che la ricetta venne rivisitata e riproposta dalle suore di un convento per celebrare il carnevale, divenendo un dolce tipico della festa. Oggi il cannolo siciliano si può trovare tutti giorni e in ogni pasticceria della Sicilia.Una cosa di cui si ha certezza, rispetto alle origini di questo dolce, è l’etimologia: il nome proviene dalle canne di fiume in cui anticamente veniva avvolto l’impasto prima della cottura, da qui il nome cannolo.',
+            "DolceQty" => '50',
+        ],
+        [
+            "DolceName" => "Cannolo siciliano",
+            "DolcePic" => 'cannollo.png',
+            "DolcePrezzo" => '5€',
+            "DolceTesto" => 'La storia del cannolo siciliano è molto antica e, come spesso accade per storie o leggende tramandate oralmente, non si è certi della sua effettiva origine.Per molti la nascita di questo delizioso dolce tipico della tradizione siciliana ha origini antiche, che risalgono addirittura all’epoca romana, attorno al 70 a.C. Molti ritengono che l’antica ricetta romana venne rivisitata dai saraceni che vissero in Sicilia, rendendo il tutto più dolce e sfizioso grazie alle importazioni di alcuni prodotti della loro cucina, come la cannella, i pistacchi ed il miele. Altri ancora sostengono che sì, l’origine sia araba, ma che la ricetta venne rivisitata e riproposta dalle suore di un convento per celebrare il carnevale, divenendo un dolce tipico della festa. Oggi il cannolo siciliano si può trovare tutti giorni e in ogni pasticceria della Sicilia.Una cosa di cui si ha certezza, rispetto alle origini di questo dolce, è l’etimologia: il nome proviene dalle canne di fiume in cui anticamente veniva avvolto l’impasto prima della cottura, da qui il nome cannolo.',
+            "DolceQty" => '50',
+        ],
+
+    ]; 
+    $Salati = [
+
+        [
+            "SalatiName" => "BABBA'",
+            "SalatiPic" => 'baba.png',
+            "SalatiPrezzo" => '3.5€',
+            "SalatiTesto" => 'Partiamo dal suono della parola, così importante per i napoletani ai quali interessa sempre anche la musicalità di una frase e di un discorso, meglio se accompagnata da gesti didascalici con le mani e ammiccamenti con gli occhi. Già, una comunicazione a tutto campo con la propria fisicità, tipica delle grandi città di un tempo visitate da genti straniere con le quali commerciare e fare affari, esigenze inutili se invece si era costretti a vivere nei campi o con le pecore o in piccoli borghi e cittadine fuori dalle rotte. Babà ha dunque nel suo suono, la seconda bi appena appena un po’ raddoppiata ma senza calcare sicché mai diventa babbà ma non è neanche babà alla francese, uno dei segreti capaci di annunciare il suo successo perchè gioca sulla piacevolezza dell’udito e sul successo della memoria: come dimenticare mai qualcosa, un Salati, con questo nome? Le prime due lettere dell’alfabeto, talmente facili da pronunciare che per i greci erano sinonimo dei popoli privi di una lingua costruita, bar bar, barbari appunto.',
+            "SalatiQty" => '30',
+        ],
+
+        [
+            "SalatiName" => "Ciambella",
+            "SalatiPic" => 'ciambella.jpeg',
+            "SalatiPrezzo" => '4€',
+            "SalatiTesto" => 'La ciambella è definita come un Salati di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "SalatiQty" => '40',
+        ],
+        [
+            "SalatiName" => "Ciambella",
+            "SalatiPic" => 'ciambella.jpeg',
+            "SalatiPrezzo" => '4€',
+            "SalatiTesto" => 'La ciambella è definita come un Salati di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "SalatiQty" => '40',
+        ],
+        [
+            "SalatiName" => "Ciambella",
+            "SalatiPic" => 'ciambella.jpeg',
+            "SalatiPrezzo" => '4€',
+            "SalatiTesto" => 'La ciambella è definita come un Salati di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "SalatiQty" => '40',
+        ],
+        [
+            "SalatiName" => "Ciambella",
+            "SalatiPic" => 'ciambella.jpeg',
+            "SalatiPrezzo" => '4€',
+            "SalatiTesto" => 'La ciambella è definita come un Salati di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "SalatiQty" => '40',
+        ],
+        [
+            "SalatiName" => "Ciambella",
+            "SalatiPic" => 'ciambella.jpeg',
+            "SalatiPrezzo" => '4€',
+            "SalatiTesto" => 'La ciambella è definita come un Salati di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "SalatiQty" => '40',
+        ],
+        [
+            "SalatiName" => "Ciambella",
+            "SalatiPic" => 'ciambella.jpeg',
+            "SalatiPrezzo" => '4€',
+            "SalatiTesto" => 'La ciambella è definita come un Salati di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "SalatiQty" => '40',
+        ],
+        [
+            "SalatiName" => "Ciambella",
+            "SalatiPic" => 'ciambella.jpeg',
+            "SalatiPrezzo" => '4€',
+            "SalatiTesto" => 'La ciambella è definita come un Salati di forma toroidale, questo termine designa anche le preparazione salate con la stessa forma. La ciambella può essere di diverse forme: di piccole dimensioni come i Donut o avere le dimensioni di una torta, detta Ciambellone specialmente in alcune aree dell’Italia centrale. La ciambella o il ciambellone è uno dei dolci fatti in casa più tipici della tradizione italiana, preparato con pasta lievitata e cotto in forno ed ha la classica forma rotonda con un buco al centro.',
+            "SalatiQty" => '40',
+        ],
+
+
+        [
+            "SalatiName" => "Cannolo siciliano",
+            "SalatiPic" => 'cannollo.png',
+            "SalatiPrezzo" => '5€',
+            "SalatiTesto" => 'La storia del cannolo siciliano è molto antica e, come spesso accade per storie o leggende tramandate oralmente, non si è certi della sua effettiva origine.Per molti la nascita di questo delizioso Salati tipico della tradizione siciliana ha origini antiche, che risalgono addirittura all’epoca romana, attorno al 70 a.C. Molti ritengono che l’antica ricetta romana venne rivisitata dai saraceni che vissero in Sicilia, rendendo il tutto più Salati e sfizioso grazie alle importazioni di alcuni prodotti della loro cucina, come la cannella, i pistacchi ed il miele. Altri ancora sostengono che sì, l’origine sia araba, ma che la ricetta venne rivisitata e riproposta dalle suore di un convento per celebrare il carnevale, divenendo un Salati tipico della festa. Oggi il cannolo siciliano si può trovare tutti giorni e in ogni pasticceria della Sicilia.Una cosa di cui si ha certezza, rispetto alle origini di questo Salati, è l’etimologia: il nome proviene dalle canne di fiume in cui anticamente veniva avvolto l’impasto prima della cottura, da qui il nome cannolo.',
+            "SalatiQty" => '50',
+        ]
+
+    ]; ?>
